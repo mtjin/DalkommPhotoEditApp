83bb79ab6984a4401cc6d11c0193e0dcf30be1de
seungeon <tmddjs210@naver.com>
2019-07-19 14:43:14 +0900

    fixes #1 로그인 부분 디자인 및 유저정보 디비저장 완료

--- Dal.kommPhoto/app/src/main/java/com/example/dalkommphoto/LoginActivity.java
+++ Dal.kommPhoto/app/src/main/java/com/example/dalkommphoto/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.dalkommphoto;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -10,9 +11,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Base64;
 import android.util.Log;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.kakao.auth.ErrorCode;
 import com.kakao.auth.ISessionCallback;
@@ -31,9 +35,12 @@ import java.util.Map;
 /**
  * Created by hp on 2016-01-26.
  */
-public class LoginActivity  extends Activity {
+public class LoginActivity extends Activity {
 
     private SessionCallback callback;      //콜백 선언
+    //유저프로필
+    String token = "";
+    String name = "";
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     final static String TAG = "LoginActivityT";
@@ -47,7 +54,11 @@ public class LoginActivity  extends Activity {
         callback = new SessionCallback();                  // 이 두개의 함수 중요함
         Session.getCurrentSession().addCallback(callback);
         getAppKeyHash();
+
+        //자기 카카오톡 프로필 정보 및 디비정보 쉐어드에 저장해놨던거 불러오기
+        loadShared();
     }
+
     private void getAppKeyHash() {
         try {
             PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
@@ -63,6 +74,7 @@ public class LoginActivity  extends Activity {
             Log.e("name not found", e.toString());
         }
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
@@ -87,19 +99,13 @@ public class LoginActivity  extends Activity {
 
         @Override
         public void onSessionOpenFailed(KakaoException exception) {
-            if(exception != null) {
+            if (exception != null) {
                 Logger.e(exception);
             }
             setContentView(R.layout.activity_login); // 세션 연결이 실패했을때
         }                                            // 로그인화면을 다시 불러옴
     }
 
-    protected void redirectSignupActivity() {       //세션 연결 성공 시 SignupActivity로 넘김
-        final Intent intent = new Intent(this, HomeActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        startActivity(intent);
-        finish();
-    }
 
     protected void requestMe() { //유저의 정보를 받아오는 함수
         UserManagement.requestMe(new MeResponseCallback() {
@@ -126,28 +132,27 @@ public class LoginActivity  extends Activity {
             } // 카카오톡 회원이 아닐 시 showSignup(); 호출해야함
 
             @Override
-            public void onSuccess(UserProfile userProfile) {  //성공 시 userProfile 형태로 반환
+            public void onSuccess(final UserProfile userProfile) {  //성공 시 userProfile 형태로 반환
                 Logger.d("UserProfile : " + userProfile);
                 Log.d(TAG, "유저가입성공");
                 // Create a new user with a first and last name
-                // 유저 카카오톡 아이디 디비에 넣음
+                // 유저 카카오톡 아이디 디비에 넣음(첫가입인 경우에만 디비에저장)
+
                 Map<String, String> user = new HashMap<>();
-                user.put("id", userProfile.getId() + "");
+                user.put("token", userProfile.getId() + "");
+                user.put("name", userProfile.getNickname());
                 db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        .document(userProfile.getId() + "")
+                        .set(user)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
-                            public void onSuccess(DocumentReference documentReference) {
+                            public void onSuccess(Void aVoid) {
                                 Log.d(TAG, "유저정보 디비삽입 성공");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.d(TAG, "유저정보 디비삽입 실패");
+                                saveShared(userProfile.getId() + "", userProfile.getNickname());
                             }
                         });
                 redirectMainActivity(); // 로그인 성공시 MainActivity로
+
             }
         });
     }
@@ -162,6 +167,22 @@ public class LoginActivity  extends Activity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         startActivity(intent);
         finish();
+    }
+
+    /*쉐어드에 입력값 저장*/
+    private void saveShared( String id, String name) {
+        SharedPreferences pref = getSharedPreferences("profile", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("token", id);
+        editor.putString("name", name);
+        editor.apply();
+    }
+
+    /*쉐어드값 불러오기*/
+    private void loadShared() {
+        SharedPreferences pref = getSharedPreferences("profile", MODE_PRIVATE);
+        token = pref.getString("token", "");
+        name = pref.getString("name", "");
     }
 
 }
