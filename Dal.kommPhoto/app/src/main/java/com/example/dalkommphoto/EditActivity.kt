1a81db7f0f0abfc518b3477b06770956ebca2d45
Edward Cho <edwardcho9762@gmail.com>
2019-07-23 12:18:14 +0900

    edit

--- Dal.kommPhoto/app/src/main/java/com/example/dalkommphoto/EditActivity.kt
+++ Dal.kommPhoto/app/src/main/java/com/example/dalkommphoto/EditActivity.kt
@@ -2,22 +2,88 @@ package com.example.dalkommphoto
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
-class EditActivity : AppCompatActivity() {
+class EditActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val ExitActivityButton = findViewById<Button>(R.id.ExitActivityButton)
+        val SavePhotoButton = findViewById<Button>(R.id.SavePhotoButton)
+
+        val imageOnEdit = findViewById<ImageView>(R.id.imageOnEdit)
+
+
+        val cutButton = findViewById<Button>(R.id.cutButton)
+        val pasteButton = findViewById<Button>(R.id.pasteButton)
+        val filterButton = findViewById<Button>(R.id.filterButton)
+
+
+        // 버튼1 => 뒤로가기 버튼: 홈 엑티비티로
+        ExitActivityButton.setOnClickListener{
+
+        }
+
+
+        // 버튼2 => 사진 데이터 베이스에 저장
+        SavePhotoButton.setOnClickListener{
+
+        }
+
+
+
+        imageOnEdit.setOnClickListener{
+
+        }
+        // 이미지 뷰 => 사진 보여주기 + 사진 편집 기능
+        // 일단 사진 보여주는 기능 빌드 업
+
+        // 사진 편집 부분
+        //      => 홈 엑티비티에서 받아온 이미지를 show
+        //      =>
+        //      * 사진 위에 작은 원 만들어서
+        //      => 원을 드레그해서, 붙인 이미지 사이즈 조정
+        //      => 원을 누르고 돌려서, 붙인 이미지 회전 시키기
+        //      => 만약 이미지가 겹치면 먼저 불러온 이미지가 아래에
+        //      => 원을 더블클릭하면, 붙인 이미지 지우기
+
+
+
+        // 오리기 버튼 => 파이썬 모듈 함수들 사용 -> db에 사진(사진들) 저장)
+        //          => 저장하기 전에 인식된 얼굴 보여주기 (팝업 메세지) 저장 확인
+        //          => 얼굴 인식이 안 되면 팝업 메세지
+        cutButton.setOnClickListener{
+
+        }
+
+
+        // 붙이기 버튼 => 슬라이드 업 판넬 생성 후 저장된 이미지 db에서 불러와 보여주기
+        //              => 옵션1  판넬에서 이미지 선택해서 사진에 띄우기
+        //              => 옵션2  판넬에서 이미지 드레그 해서 사진에 원하는 위치에 가져다 놓기
+        pasteButton.setOnClickListener{
+
+        }
+
+
+        // 필터 버튼   => 슬라이드 업 판넬 생성 후, 목록 보여주기
+        //           => 목록에서 필터 적용하면
+        //              => 사진, 붙인 이미지 함께 필터 적용
+        filterButton.setOnClickListener{
+
+        }
+
     }
 
-    // button 기능 설정
 
+    // button 기능 설정
 
     // 첫줄 버튼
     // 버튼1 => 뒤로가기 버튼: 홈 엑티비티로
 
     // 버튼2 => 사진 데이터 베이스에 저장
-
 
 
     // 두번째 줄
@@ -26,8 +92,8 @@ class EditActivity : AppCompatActivity() {
 
     // 사진 편집 부분
     //      => 홈 엑티비티에서 받아온 이미지를 show
-    //      => 
-    //      => 사진 위에 작은 원 만들어서
+    //      =>
+    //      * 사진 위에 작은 원 만들어서
     //      => 원을 드레그해서, 붙인 이미지 사이즈 조정
     //      => 원을 누르고 돌려서, 붙인 이미지 회전 시키기
     //      => 만약 이미지가 겹치면 먼저 불러온 이미지가 아래에
@@ -48,9 +114,7 @@ class EditActivity : AppCompatActivity() {
 
     // 필터 버튼   => 슬라이드 업 판넬 생성 후, 목록 보여주기
     //           => 목록에서 필터 적용하면
-    //
-    //
-
+    //              => 사진, 붙인 이미지 함께 필터 적용
 
 
 }
